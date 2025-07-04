5cb9416f12468320ea32ed3142e93c536f7d4b3f
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-07-04 17:03:42 -0600

    🚀 first commit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://admin:Nirvana3@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando.....')
   const animales = await Animal.find();
   return res.send(animales)
 })
